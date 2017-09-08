77dfff1a89a5b5cd060ba884038b05a9135357be
Robert Peck <rspeckpfs@gmail.com>
2017-09-08 07:10:23 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,11 +13,9 @@ end
 
 def contain_a(array)
   output = []
-  array.each do |x|
-    if x.chars.sort(&:casecmp)[0] == "a"
-      output << x
+    array.each do |x|
+      output << x if x.include?("a")
     end
-  end
   output
 end
 
@@ -40,16 +38,16 @@ end
 def count_elements(array) #this doesnt all make sense yet, but its what I found online when I couldnt figure it out. It mostly makes sense
   foo = array.group_by(&:itself) #group_by(&:itself) groups like with like
   foo.map {|k, v| k.merge(count: v.length)} #merge will 'add' the 'count' variable to each current 'k', equal to v.length. k is each unique element, v is the array of all instances of that unique element.
-end
+end                       #will merge k hash with whats inside ( ), in this case a hash of {count: v.length}
 
-def merge_data(keys, data)
-  keys[0].values.map.with_index {|k, i| data[i].merge(k)}
-end
+# def merge_data(keys, data)
+#   keys[0].values.map.with_index {|k, i| data[i].merge(k)} #found online, still slightly above my comprehension level, redid it below.
+# end
 
 def merge_data(keys, data)
   output = []
   keys.each {|element|
-    element.each {|key, value| #Check on same name, ad to corresponding data hash
+    element.each {|key, value| #Check on same name, add to corresponding data hash
       data.each {|names|
         names.each {|name, data_list| #data_list is attribute list in 'data'.
           if name == value
